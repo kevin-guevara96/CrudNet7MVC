bd1acacac1ee885f1032b08a3a5c945f8e43f9a6
Sistemas-KG <Sistemas-KG@DESKTOP-0L9KE8I>
2023-11-13 09:46:21 -0600

    Agregar archivos de proyecto.

---
+++ CrudNet7MVC/Controllers/InicioController.cs
@@ -0,0 +1,156 @@
+﻿using CrudNet7MVC.Datos;
+using CrudNet7MVC.Models;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MimeKit;
+using System.Diagnostics;
+
+namespace CrudNet7MVC.Controllers
+{
+    public class InicioController : Controller
+    {
+        private readonly ApplicationDbContext _contexto;
+
+        public InicioController(ApplicationDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {            
+
+            return View(await _contexto.Contacto.ToListAsync());
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Add(contacto);
+                await _contexto.SaveChangesAsync();
+
+                //Para envío de correo
+                var mensaje = new MimeMessage();
+                mensaje.From.Add(new MailboxAddress("Test envio email", "app.comintec@gmail.com"));
+                mensaje.To.Add(new MailboxAddress("Test enviado", "sistemas2@comintec.com.mx"));
+                mensaje.Subject = "Test email asp.net core 7";
+                mensaje.Body = new TextPart("plain")
+                {
+                    Text = "Hola saludo desde  asp.net core mai"
+                };
+
+                using (var cliente = new SmtpClient())
+                {
+                    cliente.Connect("smtp.gmail.com", 587);
+                    cliente.Authenticate("app.comintec@gmail.com", "gvnzchqtrvuxolux");
+                    cliente.Send(mensaje);
+                    cliente.Disconnect(true);
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(Contacto contacto)
+        {
+            if (ModelState.IsValid)
+            {
+                _contexto.Contacto.Update(contacto);
+                await _contexto.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpGet]
+        public IActionResult Borrar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contacto = _contexto.Contacto.Find(id);
+
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+
+            return View(contacto);
+        }
+
+        [HttpPost, ActionName("Borrar")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> BorrarContacto(int? id)
+        {
+            var contacto = await _contexto.Contacto.FindAsync(id);
+            if (contacto == null)
+            {
+                return View();
+            }
+
+            //Borrado
+            _contexto.Contacto.Remove(contacto);
+            await _contexto.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CrudNet7MVC/Datos/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudNet7MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudNet7MVC.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        //Agregar los modelos aquí
+        public DbSet<Contacto> Contacto { get; set; }
+    }
+}
